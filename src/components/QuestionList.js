b226beacefb9dc1aa37d3e304f60793a38fc2754
Joy Chela <joychela000@gmail.com>
2024-08-06 18:42:31 +0300

    CRUD using fetch

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data))
+      .catch((error) => console.error("Error fetching questions:", error));
+  }, []);
+
+  const handleDelete = (id) => {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.ok) {
+          setQuestions((questions) => questions.filter((question) => question.id !== id));
+        } else {
+          console.error("Failed to delete the question");
+        }
+      })
+      .catch((error) => console.error("Error deleting question:", error));
+  };
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem key={question.id} question={question} onDelete={handleDelete} />
+        ))}
+      </ul>
     </section>
   );
 }
